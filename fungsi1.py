247f343c1e49d7dcb29299969af2275d22e2985d
Afifaftawi <afifaftawi07@gmail.com>
2024-11-12 14:29:42 +0800

    Create fungsi1.py

---
+++ fungsi1.py
@@ -0,0 +1,10 @@
+#fungsi dan modularisasi
+#f (x)= 5x-10
+def fungsinya (x):
+    return (5*x)-10
+
+a=int(input("masukan nilai x :"))
+
+#hasil fx jika nlai x=10 
+hasil_fx = fungsinya(a)
+print (hasil_fx)
